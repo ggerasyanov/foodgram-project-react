dca0500bd8f3351056a36b075b5910061d10bebf
Герман <QieDie@yandex.ru>
2022-05-05 14:55:12 +0300

    change settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,14 +72,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
@@ -88,7 +82,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
-} """
+}
 
 
 # Password validation
