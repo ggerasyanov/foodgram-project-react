d8d214e6890ff69baab6f46dda393608a8e5fe9b
Герман <QieDie@yandex.ru>
2022-05-09 15:42:36 +0300

    correct settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -137,11 +137,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend'),
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
+    ),
 }
 
 
