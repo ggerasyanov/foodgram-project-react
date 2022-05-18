8779965cc02ebd1a9399deb03c8d982fb8cd502d
Герман <QieDie@yandex.ru>
2022-05-18 15:42:41 +0300

    correct import

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from requests import Response
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from users.models import Follow, User
 from .serializers import FollowSerializer
